35d8d5587a72a2731529d05ccb3d1545ac781c87
Taeho Kim <jyte82@gmail.com>
2013-05-22 15:59:58 +0900

    Translated template.html

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: '슬라이드 제목<br/>두줄까지 지원됩니다.',
+    subtitle: '부제목은 여기에 표시됩니다.',
     //eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
@@ -12,19 +12,21 @@ var SLIDE_CONFIG = {
     favIcon: 'images/google_developers_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
-      'Source Code Pro'
+      'Source Code Pro',
+      'Malgun Gothic',
+      'Nanum Gothic',
     ],
     //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: '김태호',
+    company: '커니의 안드로이드 이야기',
+    gplus: 'http://plus.google.com/109811245384932244476',
+    twitter: '@Androidhuman',
+    www: 'http://androidhuman.tistory.com',
+    github: 'http://github.com/kunny'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
